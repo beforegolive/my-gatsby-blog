a794a7c10f98a2e346d146b06fdffa321b5ffa96
twomeetings <2326220@163.com>
2021-09-18 05:02:33 +0800

    start second part

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,48 @@
-import React, { useEffect } from 'react'
-import { Link } from 'gatsby'
+import React, { useEffect } from "react"
+import { Link } from "gatsby"
 
-import Header from './header'
-import { rhythm, scale } from '../utils/typography'
-import './layout.scss'
+import Header from "./header"
+import { rhythm, scale } from "../utils/typography"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
-	useEffect(() => {
-		async function setHighlightCode() {
-			try {
-				const deckdeckgoHighlightCodeLoader = require('@deckdeckgo/highlight-code/dist/loader')
+  useEffect(() => {
+    async function setHighlightCode() {
+      try {
+        const deckdeckgoHighlightCodeLoader = require("@deckdeckgo/highlight-code/dist/loader")
 
-				await deckdeckgoHighlightCodeLoader.defineCustomElements(window)
-			} catch (err) {
-				console.error(err)
-			}
-		}
+        await deckdeckgoHighlightCodeLoader.defineCustomElements(window)
+      } catch (err) {
+        console.error(err)
+      }
+    }
 
-		setHighlightCode()
-	})
+    setHighlightCode()
+  })
 
-	const rootPath = `${__PATH_PREFIX__}/`
-	return (
-		<div
-			style={{
-				marginLeft: `auto`,
-				marginRight: `auto`,
-				maxWidth: rhythm(32),
-				padding: `0 ${rhythm(1.5)} ${rhythm(3 / 4)}`
-			}}
-			className="post-container"
-		>
-			<Header></Header>
-			<main>{children}</main>
-			<footer className="footer">
-				<span>© {new Date().getFullYear()} 深圳市悦初科技有限公司</span>
-				<span>粤ICP备2020110796号</span>
-				<span>Built with</span>
-				<a href="https://www.gatsbyjs.org">Gatsby</a>
-				<a href="https://gitalk.github.io/">Gitalk</a>
-			</footer>
-		</div>
-	)
+  const rootPath = `${__PATH_PREFIX__}/`
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(32),
+        padding: `0 ${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+      className="post-container"
+    >
+      <Header></Header>
+      <main>{children}</main>
+      <footer className="footer">
+        <span>
+          <a href="https://beian.miit.gov.cn/">粤ICP备2020110796号</a>
+        </span>
+        <span>Built with</span>
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <a href="https://gitalk.github.io/">Gitalk</a>
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
